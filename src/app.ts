42c6ed0ff31bdd27349ad5467a3fea16c638c14f
xymon2 <vbbn2030@kaist.ac.kr>
2022-02-09 17:35:46 +0900

    delete server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('./config');
+    // this should be imported before importing any ohter files
+}
+
 import express, { Express, Request, Response, NextFunction } from 'express';
 
-export default async (): Promise<Express> => {
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+
+const appSetting = async (): Promise<Express> => {
     const app = express();
     const urlPrefix = '/api';
     //db connection
@@ -20,3 +27,33 @@ export default async (): Promise<Express> => {
 
     return app;
 };
+
+// run nodejs server
+const initServer = async () => {
+    try {
+        const app: Express = await appSetting();
+        const server = app
+            .listen(PORT, () => {
+                console.log(`
+        ################################################
+        🛡️  Server listening on port: ${PORT}🛡️
+        ################################################
+        `);
+            })
+            .on('error', (err: Error) => {
+                console.error(err);
+                process.exit(1);
+            });
+        process.on('SIGINT', () => {
+            console.log('Received SIGINT/Ctrl+C, shutting down...');
+            server.close();
+
+            // db 연결해제 추가하셈;
+            process.exit(130);
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+initServer();
